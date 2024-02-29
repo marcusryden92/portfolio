5342be5f81b9184aeecc4590ef46baf470093a87
marcusryden92 <marcus.ryden1992@gmail.com>
2024-02-29 19:14:00 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "./index.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-500-blue">Marcus</div>
+    </div>
+  );
 }
 
 export default App;
