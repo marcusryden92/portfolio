cf248a2480b7890dc9f8e408f1e5b9b154bb9977
marcusryden92 <marcus.ryden1992@gmail.com>
2024-03-01 02:45:36 +0100

    mmm

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -19,7 +19,7 @@ export default function Cover() {
         <div className="josefin-sans text-8xl  font-bold text-stone-600	hover:text-stone-500 ease-in-out duration-500">
           Marcus Rydén
         </div>
-        <p className=" league-script-regular text-5xl mt-10 text-500-slate font-extralight">
+        <p className="text-5xl mt-10 text-500-slate font-extralight">
           Portfolio
         </p>
       </div>
