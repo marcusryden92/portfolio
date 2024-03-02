d4ad82976da77af00a7ebc8752e82e6a7c7c7bee
marcusryden92 <marcus.ryden1992@gmail.com>
2024-03-02 21:32:31 +0100

    .

---
+++ src/context/ContextWebD.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useEffect, useState } from "react";
+export const ContextProvider = createContext(null);
+
+export const ContextWebD = ({ children }) => {
+  const [isVisibleWebD, setIsVisibleWebD] = useState(false);
+  const value = {
+    isVisibleWebD,
+    setIsVisibleWebD,
+  };
+
+  return (
+    <ContextProvider.Provider value={value}>
+      {children}
+    </ContextProvider.Provider>
+  );
+};
+
+export const CustomContextWebD = () => {
+  const context = useContext(ContextProvider);
+  if (!context) {
+    throw new Error("customContextWebD undefined");
+  }
+  return context;
+};
