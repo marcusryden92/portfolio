b2f28a98ee6baf1ce12ded1f492c505d559d344a
marcusryden92 <marcus.ryden1992@gmail.com>
2024-03-02 19:33:47 +0100

    ..

--- src/pages/WebD.jsx
+++ src/pages/WebD.jsx
@@ -1,15 +1,30 @@
 import GalleryContainer from "../components/GalleryContainer";
 import { useState, useEffect } from "react";
+import { customContext } from "../context/Context";
 
-export default function WebD({ handleClickClear, isClickedWebD }) {
+export default function WebD({ handleClickClear }) {
+  const { isClickedWebD } = customContext();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(isClickedWebD);
+    console.log(isClickedWebD);
+  }, [isClickedWebD]);
+
   return (
     <>
       <div
         className="box  my-5 mx-20 p-10 text-left"
         onClick={handleClickClear}
       >
-        <div className="my-4">Webdesign</div>
-
+        <div
+          className={`${
+            isVisible ? "gallery-title" : "gallery-title--faded"
+          } my-4 bg-black text-white w-32 p-1 rounded-xl text-center 
+        font-medium hover:scale-110 ease-in-out duration-1000`}
+        >
+          Webdesign
+        </div>
         <GalleryContainer isClickedWebD={isClickedWebD} />
       </div>
     </>
