f4dbedd97db38777d929369afd27afc098bba03c
marcusryden92 <marcus.ryden1992@gmail.com>
2024-03-02 23:26:55 +0100

    color stuff

--- src/components/InteractiveCircles.jsx
+++ src/components/InteractiveCircles.jsx
@@ -24,13 +24,44 @@ const InteractiveCircles = () => {
 
     const BACKGROUNDCOLOR = "#999999";
 
-    const colorArray = [
+    const COLORUPDATERATE = 0;
+
+    /* const colorArray = [
       "#ED6F47",
       "#EDA947",
       "#ED8C47",
       "#F56055",
       "#EDBD47",
       "#000000",
+    ];*/
+
+    /*const colorArray = [
+      "hsl(20, 100%, 60%)", // #ED6F47
+      "hsl(35, 100%, 60%)", // #EDA947
+      "hsl(30, 100%, 60%)", // #ED8C47
+      "hsl(5, 100%, 60%)",  // #F56055
+      "hsl(50, 100%, 60%)", // #EDBD47
+      "hsl(0, 0%, 0%)"     // #000000
+    ];*/
+
+    //Color Black and White:
+
+    /*   const colorArray = [
+      [20, 0, 90], // #ED6F47
+      [35, 0, 85], // #EDA947
+      [30, 0, 75], // #ED8C47
+      [5, 0, 55], // #F56055
+      [50, 0, 40], // #EDBD47
+      [0, 0, 0], // #000000
+    ]; */
+
+    const colorArray = [
+      [20, 100, 60], // #ED6F47
+      [35, 100, 60], // #EDA947
+      [30, 100, 60], // #ED8C47
+      [5, 100, 60], // #F56055
+      [50, 100, 60], // #EDBD47
+      [0, 0, 0], // #000000
     ];
 
     const resizeCanvas = () => {
@@ -55,10 +86,13 @@ const InteractiveCircles = () => {
         dy,
         radius,
         color: colorArray[Math.floor(Math.random() * colorArray.length)],
+
         draw: function () {
           c.beginPath();
           c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-          c.fillStyle = this.color;
+          c.fillStyle = `hsl(${Math.floor(this.color[0])}, ${this.color[1]}%, ${
+            this.color[2]
+          }%)`;
           c.fill();
         },
         update: function () {
@@ -91,6 +125,12 @@ const InteractiveCircles = () => {
             this.radius = MAXRADIUS * proximityFactor + baseRadius;
           } else {
             this.radius = baseRadius;
+          }
+
+          if (this.color[0] < 360) {
+            this.color[0] += COLORUPDATERATE;
+          } else {
+            this.color[0] = 0;
           }
 
           this.draw();
