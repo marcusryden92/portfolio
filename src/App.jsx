16d091ef098cbcf35fca32e7eee28b8826c08adb
Marcus Rydén <marcus.ryden1992@gmail.com>
2024-03-02 04:21:10 +0100

    Added cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import "./index.css";
+import CustomCursor from "./components/CustomCursor";
 
 import MainPage from "./pages/MainPage";
 
 function App() {
-  return <MainPage />;
+  return (
+    <>
+      <MainPage />
+      <CustomCursor />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/CustomCursor.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import CursorSVG from "../assets/cursor.svg";
+import "../index.css"; // Optional: You can define styles for your cursor in a separate CSS file
+
+const CustomCursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div
+      className="custom-cursor"
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+        display:
+          position.y <= 10 || position.y >= innerHeight - 10 ? "none" : "block", // Hide cursor when y position is 0
+      }}
+    >
+      <img src={CursorSVG} alt="Custom Cursor" />
+    </div>
+  );
+};
+
+export default CustomCursor;
