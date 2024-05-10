08a31c5f4eb52bf6499183a7e77397dfc13dc241
Marcus Rydén <marcus.ryden1992@gmail.com>
2024-05-10 21:29:54 +0200

    ..

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Cover from "../components/Cover";
 import InteractiveCircles from "../components/InteractiveCircles";
