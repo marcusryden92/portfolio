f3bbd115e61df30107b332043a8004ef004dd8b8
marcusryden92 <marcus.ryden1992@gmail.com>
2024-05-11 21:37:12 +0200

    ..

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import { useContextAbout } from "../context/ContextAbout";
 import { useEffect, useState } from "react";
 import { usePageNav } from "../hooks/usePageNavigation";
 import portrait from "../../public/images/portrait_mono.png";
+import gecko from "../../public/images/gecko.png";
 
 export default function About({ handleClickClear }) {
   const { isFadedAbout, setIsFadedAbout } = useContextAbout();
@@ -57,7 +58,18 @@ export default function About({ handleClickClear }) {
         >
           <div className="flip-card-inner">
             <div className="flip-card-front overflow-hidden flex">
-              <div className="flex p-10 flex-grow">About - Click to flip</div>
+              <div className="flex flex-col">
+                <div className="flex p-10 flex-grow">
+                  Hello! I'm Marcus. <br /> I've studied industrial design at
+                  LTH, and more recently front-end development at Hyper Island.
+                  In my spare time I try to exercise and read difficult books,
+                  with some assistance from my pet leopard gecko Scheherazade.
+                </div>
+                <img
+                  src={gecko}
+                  className="h-1/2 w-1/2 object-contain p-5 items-end"
+                />
+              </div>
               <img
                 className="md:h-[35vw] self-end"
                 src={portrait}
