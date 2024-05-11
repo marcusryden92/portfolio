e0bc36c4f9f85470cfdcac28cf09101ff04afc07
marcusryden92 <marcus.ryden1992@gmail.com>
2024-05-12 00:13:54 +0200

    cover

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -11,12 +11,13 @@ export default function Cover() {
   }
   return (
     <div
+      onClick={handleFade}
       className={`flex flex-col cover-viewport cover-container flex-center bg-white
     ${isFade ? "fade-out" : "front-page"} 
     ${isHidden ? "hidden" : ""}`}
     >
-      <div className="" onClick={handleFade}>
-        <div className="josefin-sans text-8xl  font-bold text-stone-600	hover:text-slate-400 ease-in-out duration-500">
+      <div className="">
+        <div className="josefin-sans text-8xl  font-bold text-neutral-950	hover:text-neutral-600 ease-in-out duration-500">
           Marcus Rydén
         </div>
         <p className="text-5xl mt-5 text-500-slate font-extralight">
